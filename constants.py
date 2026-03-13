91fa2de9ac824b4d796d1ecc9e7d3db704d9afe4
Mbapat2211 <22mihirbapat11@gmail.com>
2026-03-13 17:51:58 +0000

    Create Operation

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 import generate_hash as gh
 
+DATABASE_NAME = "Customer.db"
 TABLE_NAME = "CUSTOMER_DATA"
 ACCESS_TOKEN = gh.HASHED
---
+++ create.py
@@ -0,0 +1,7 @@
+import sqlite3 as sql
+import constants
+
+def create(name, age, email):
+    con = sql.connect(constants.DATABASE_NAME)
+    c = con.cursor()
+    c.execute(f"INSERT INTO {constants.TABLE_NAME} VALUES (?, ?, ?)", (name, age, email))
